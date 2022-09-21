3dfa210b05ef0f50ac77f96bb7aa22db92e8da8f
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-21 14:10:50 -0700

    Part A: Display countries

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 /* Imports */
 // > Part A: Import `getCountries` from fetch-utils.js
+import { getCountries } from './fetch-utils.js';
 // > Part B: Import `getContinents` from fetch-utils.js
-import { renderContinentOption, renderCountry } from './render-utils.js';
+import { renderCountry } from './render-utils.js';
 
 /* Get DOM Elements */
 const notificationDisplay = document.getElementById('notification-display');
 const searchForm = document.getElementById('search-form');
-const continentSelect = document.getElementById('continent-select');
+// const continentSelect = document.getElementById('continent-select');
 const countryList = document.getElementById('country-list');
 
 /* State */
 let error = null;
-let count = 0;
+// let count = 0;
 let continents = [];
 let countries = [];
 
 /* Events */
 window.addEventListener('load', async () => {
     // > Part A: call findCountries (with no arguments)
+    findCountries();
 
     // > Part B: await the call to get continents to get the response
 
@@ -30,14 +32,18 @@ window.addEventListener('load', async () => {
     }
 });
 
-async function findCountries(name, continent) {
+async function findCountries() {
     // > Part A: Call the service function that gets the countries
+    const response = await getCountries();
+    console.log(getCountries);
 
     // > Part C: Add the name and continent arguments to getCountries
 
     // > Part A: Assign to state the :
     //      - error,
     //      - data (to the countries variable)
+    error = response.error;
+    countries = response.data;
 
     // > Part D: Assign to state the:
     //      - count (of db records)
@@ -60,6 +66,8 @@ function displayCountries() {
 
     for (const country of countries) {
         // > Part A: render and append to list
+        const countryEl = renderCountry(country);
+        countryList.append(countryEl);
     }
 }
 
