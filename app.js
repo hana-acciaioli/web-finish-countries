c46e7fbc573bcd3150d800b2a5034bbd736629da
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-21 14:46:22 -0700

    Part C: Search

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ window.addEventListener('load', async () => {
     }
 });
 
-async function findCountries() {
+async function findCountries(name, continent) {
     // > Part A: Call the service function that gets the countries
-    const response = await getCountries();
+    const response = await getCountries(name, continent);
 
     // > Part C: Add the name and continent arguments to getCountries
 
@@ -58,6 +58,7 @@ searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(searchForm);
     // > Part C: Call findCountries with name and continent from formData
+    findCountries(formData.get('name'), formData.get('continent'));
 });
 
 /* Display Functions */
